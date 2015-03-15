9475230bf134edce2d5d3ad50404d9958b3e31c0
Axel Tetzlaff <axel.tetzlaff@gmx.de>
2015-03-15 03:41:01 +0100

    added dance taxonomy

--- app/models/dance.rb
+++ app/models/dance.rb
@@ -1,3 +1,49 @@
 class Dance < ActiveRecord::Base
   validates :name, presence: true
+  belongs_to :dance # parent dance genre
+
+  class DanceTree
+    attr_reader :children
+    def initialize(parent, dance)
+      @parent = parent
+      @dance = dance
+    end
+
+    def assign_children(parent, all_dances)
+      my_children = all_dances.select do |dance|
+        dance.dance == @dance
+      end
+      @children = my_children.map do |dance|
+        DanceTree.new(self, dance)
+      end
+      @children.each do |child|
+        child.assign_children(self, all_dances)
+      end
+    end
+
+    def ancestors
+      if @parent
+        [@parent] + @parent.ancestors
+      else
+        []
+      end
+    end
+
+    def name
+      ancestor_names = ancestors.map(&:name)
+      full_name = @dance.try(:name) || ''
+      ancestor_names.inject(full_name) do |sum, anc_name|
+        sum.gsub(anc_name, '')
+      end
+    end
+
+  end
+
+  def self.tree
+    all = Dance.all.to_a
+    root = DanceTree.new(nil, nil)
+    root.assign_children(root, all)
+    root
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+def create_dances(children, parent)
+  case children
+  when String
+    Dance.create(name: children.to_s, dance: parent)
+  when Symbol
+    Dance.create(name: children.to_s, dance: parent)
+  when Array
+    children.each do |child|
+      create_dances(child, parent)
+    end
+  when Hash
+    children.each do |sub_grenre, sub_children|
+      new_parent = Dance.create(name: sub_grenre, dance: parent)
+      create_dances(sub_children, new_parent)
+    end
+  end
+
+end
+DatabaseCleaner.clean_with :truncation
+{
+  partner: {
+    standard: ['waltz', 'quickstep', 'foxtrott', 'tango', 'viennese waltz'],
+    latin: {
+      'latin club' => {
+        salsa: {
+          'cuban salsa' => nil,
+          'linear salsa' => ['salsa NY style', 'salsa LA style']
+       },
+        bachata: nil,
+
+      },
+      'latin ballroom' => ['cha cha', 'rumba', 'jive', 'samba', 'paso doble'],
+    },
+    bolero: nil
+  },
+  nonpartner: {
+    jazz: nil,
+    'hip hop' => nil
+  }
+}.each do |top_genre, sub_grenres|
+  parent = Dance.create(name: top_genre)
+  create_dances(sub_grenres, parent)
+end
+
